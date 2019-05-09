0937e12180846fff6a354afe260c911860ddee72
CharlieWangSI <charlie.hongyu.wang@gmail.com>
2019-05-09 17:32:30 -0400

    deploy-to-production checkpoint

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -8,11 +8,29 @@ describe("routes : static", () => {
   describe("GET /", () => {
 
 //#2
-    it("should return status code 200", (done) => {
+    it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", () => {
 
 //#3
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
+        expect(body).toContain("Welcome to Bloccit");
+
+//#4
+        done();
+      });
+    });
+
+  });
+
+  describe("GET /marco", () => {
+
+//#2
+    it("should return status code 200", (done) => {
+
+//#3
+      request.get(marco, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        expect(body).toBe("polo");
 
 //#4
         done();
