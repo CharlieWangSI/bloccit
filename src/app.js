257256247ab49186b3e9b58290a11fc243f82844
CharlieWangSI <charlie.hongyu.wang@gmail.com>
2019-06-28 19:11:09 -0400

    authentication failed try

--- src/app.js
+++ src/app.js
@@ -3,8 +3,11 @@ const app = express();
 const appConfig = require("./config/main-config.js");
 
 const routeConfig = require("./config/route-config.js");
-
-
+// const ejslint= require("ejs-lint");
+const view = require("./views/static/index.ejs");
+// console.log("linting view");
+// ejslint.lint(view);
+// console.log("linted");
  appConfig.init(app, express);
  routeConfig.init(app);
 
